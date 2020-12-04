18ee5166650d6bb0e2cf1e3b1f6bbeebb3501b5e
Junyuan Chen <junyuan.chen.c@gmail.com>
2020-12-04 18:08:39 -0500

    Add published_at to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_220325) do
+ActiveRecord::Schema.define(version: 2020_12_04_224344) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_11_28_220325) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "approver_id"
     t.boolean "hidden", default: false, null: false
+    t.datetime "published_at", precision: 6
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
