444b8ec9e9f708ba643fd53820e2c674f829680d
Junyuan Chen <junyuan.chen.c@gmail.com>
2020-12-11 15:02:57 -0500

    Use CDN for Turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,23 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("turbolinks").start()
-
 import '../css/main.scss'
-
 require.context('../img', true)
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
 
 $(document).on('turbolinks:load', function () {
     $('.alert.alert-dismissible').click(function () {
-        this.remove();
+        this.remove()
     })
 
     $('.markdown-content table:not(.style-as-is)').addClass('table table-striped table-hover')
